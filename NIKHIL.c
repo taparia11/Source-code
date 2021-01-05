1c36ffdaf888c0f243d3e13f92d00a85fe786796
Avik2903 <76903360+Avik2903@users.noreply.github.com>
2021-01-05 17:53:15 +0530

    abc

--- NIKHIL.c
+++ NIKHIL.c
@@ -1,9 +1,27 @@
 #include <stdio.h>
-
-int main()
-{
-   int a,b;
-   scanf("%d",&a); 
-    printf("%d",a+2);
-    return 0;
+int main() {
+   int arr[100],t,k,x,a,b,c,d,i,j,p;
+  scanf("%d",&t);
+  for(k=0;k<t;k++)
+  {
+  scanf("%d %d",&a,&b);
+    for(x=0;x<a;x++)
+    {
+    scanf("%d",&arr[x]);
+    }
+   for(i=0;i<a;i++)
+   {
+       for(j=0;j<a;j++)
+        {
+            for ( p = 0; p < a; p++)
+            {
+                if(arr[i]+arr[j]+arr[p]==b)
+                c++;
+            }
+            
+        }
+   } 
+   
+  }
+  return 0;
 }
