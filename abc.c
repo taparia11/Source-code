ac79903ca7d26507d20753afeaf67a6edf61bf67
Avik2903 <76903360+Avik2903@users.noreply.github.com>
2021-01-06 12:57:14 +0530

    buying sweets

--- abc.c
+++ abc.c
@@ -1,14 +1,45 @@
 #include <stdio.h>
-int main() {
-  int n, i, j;
-  scanf("%d", &n);
-  for( i = 1; i <= n; i++)
-  {
-  for( j = 1; j <= i; j++)
-  {
-    printf("*");
-  }
-    printf("\n");
-  }
+struct banknotes
+{
+  int n,x,notes[100];
+};
+int main(void) {
+  struct banknotes check;
+	int t,i;
+	scanf("%d",&t);
+	while(t--)
+	{
+	    int s=0;
+	    scanf("%d %d",&check.n,&check.x);
+	    for(i=0;i<check.n;i++)
+	    {
+	        scanf("%d",&check.notes[i]);
+	    }
+	    int m=0;
+	    for(i=0;i<check.n;i++)
+	    {
+	        s=s+check.notes[i];
+	    }
+	    if(s%check.x == 0)
+	    {
+	    printf("%d\n",s/check.x);
+	    }
+	    else
+	    {
+	        for(i=0;i<check.n;i++)
+	        {
+	            if(check.notes[i] <= s%check.x)
+	            m=1;
+	        }
+	        if(m==1)
+	        {
+	            printf("-1\n");
+	        }
+	        else
+	        {
+	            printf("%d\n",s/check.x);
+	        }
+	    }
+	}
 	return 0;
 }
