857f02a81f0bdfb23e728e450483d4649c6eff8b
Avik2903 <76903360+Avik2903@users.noreply.github.com>
2021-01-27 09:42:19 +0530

    hii

--- source.c
+++ source.c
@@ -8,44 +8,35 @@ int main()
 {
  int n,i,j;
  void queen(int row,int n);
- 
- printf(" - N Queens Problem Using Backtracking -");
- printf("\n\nEnter number of Queens:");
  scanf("%d",&n);
  queen(1,n);
  return 0;
 }
- 
-//function for printing the solution
 void print(int n)
 {
  int i,j;
- printf("\n\nSolution %d:\n\n",++count);
- 
+ //printf("\n\nSolution %d:\n\n",++count);
+ //printf("\n\n");
  for(i=1;i<=n;++i)
-  printf("\t%d",i);
+  //printf(" ");
  
  for(i=1;i<=n;++i)
  {
-  printf("\n\n%d",i);
-  for(j=1;j<=n;++j) //for nxn board
+  printf("\n\n");
+  for(j=1;j<=n;++j) 
   {
    if(board[i]==j)
-    printf("\tQ"); //queen at i,j position
+    printf(" Q"); 
    else
-    printf("\t-"); //empty slot
+    printf(" -"); 
   }
  }
 }
- 
-/*funtion to check conflicts
-If no conflict for desired postion returns 1 otherwise returns 0*/
 int place(int row,int column)
 {
  int i;
  for(i=1;i<=row-1;++i)
  {
-  //checking column and digonal conflicts
   if(board[i]==column)
    return 0;
   else
@@ -53,10 +44,8 @@ int place(int row,int column)
     return 0;
  }
  
- return 1; //no conflicts
+ return 1; 
 }
- 
-//function to check for proper positioning of queen
 void queen(int row,int n)
 {
  int column;
@@ -64,10 +53,10 @@ void queen(int row,int n)
  {
   if(place(row,column))
   {
-   board[row]=column; //no conflicts so place queen
-   if(row==n) //dead end
-    print(n); //printing the board configuration
-   else //try queen with next position
+   board[row]=column; 
+   if(row==n) 
+    print(n); 
+   else
     queen(row+1,n);
   }
  }
