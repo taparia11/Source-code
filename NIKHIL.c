6f077fa5d9e81d3be4ffa408e7648452f7ec54d1
NIKHIL JI <taparianikhil13@gmail.com>
2021-01-27 09:44:17 +0530

    n queen net version

--- NIKHIL.c
+++ NIKHIL.c
@@ -1,29 +1,74 @@
-#include <stdio.h>
-int main() {
-   int arr[100],t,k,x,a,b,c=0,d,i,j,p;
-  scanf("%d",&t);
-  for(k=0;k<t;k++)
+#include<stdio.h>
+#include<math.h>
+#include<stdlib.h>
+ 
+int board[20],count;
+ 
+int main()
+{
+ int n,i,j;
+ void queen(int row,int n);
+ 
+ printf(" - N Queens Problem Using Backtracking -");
+ printf("\n\nEnter number of Queens:");
+ scanf("%d",&n);
+ queen(1,n);
+ return 0;
+}
+ 
+//function for printing the solution
+void print(int n)
+{
+ int i,j;
+ printf("\n\nSolution %d:\n\n",++count);
+ 
+ for(i=1;i<=n;++i)
+  printf("\t%d",i);
+ 
+ for(i=1;i<=n;++i)
+ {
+  printf("\n\n%d",i);
+  for(j=1;j<=n;++j) //for nxn board
   {
-  scanf("%d %d",&a,&b);
-    for(x=0;x<a;x++)
-   //avik 
-   {
-    scanf("%d",&arr[x]);
-    }
-   for(i=0;i<a-2;i++)
-   {
-       for(j=i+1;j<a-1;j++)
-        {
-            for ( p = j+1; p < a; p++)
-            {
-                if(arr[i]+arr[j]+arr[p]==b)
-                c++;
-            }
-            
-        }
-   } 
-   printf("%d\n",c);
-   c=0;
+   if(board[i]==j)
+    printf("\tQ"); //queen at i,j position
+   else
+    printf("\t-"); //empty slot
   }
-  return 0;
+ }
+}
+ 
+/*funtion to check conflicts
+If no conflict for desired postion returns 1 otherwise returns 0*/
+int place(int row,int column)
+{
+ int i;
+ for(i=1;i<=row-1;++i)
+ {
+  //checking column and digonal conflicts
+  if(board[i]==column)
+   return 0;
+  else
+   if(abs(board[i]-column)==abs(i-row))
+    return 0;
+ }
+ 
+ return 1; //no conflicts
+}
+ 
+//function to check for proper positioning of queen
+void queen(int row,int n)
+{
+ int column;
+ for(column=1;column<=n;++column)
+ {
+  if(place(row,column))
+  {
+   board[row]=column; //no conflicts so place queen
+   if(row==n) //dead end
+    print(n); //printing the board configuration
+   else //try queen with next position
+    queen(row+1,n);
+  }
+ }
 }
