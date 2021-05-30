82841c8cd3ed189112775b1811718e35c9d209f5
NIKHIL JI <taparianikhil13@gmail.com>
2021-05-30 13:08:48 +0530

    ok

--- Tem.c
+++ Tem.c
@@ -1,64 +1,36 @@
 #include <stdio.h>
-#include <string.h>
-
-int main()
-
+struct student
 {
-        int i,a,b,c,j,e=0,f=0,g=0,h,d[100];
-        char str[100],st[10];
-        scanf("%s",str);
-        a=strlen(str);
-        for(i=0;i<a;i++)
-        {
-                if(str[i]>47 && str[i]<58)
-           {    
-                   st[f]=str[i];
-                   e++;f++;
-                   goto L1;
-           }
-
-                if(e>0)
-                {
-                        for(int j=i-1;j>i-e;j--)
-                        if(g<=f)
-                        {
-                        str[j]=st[g];
-                        g++;
-                        }
-                }
-                f=0;e=0;
-                if(str[i] < 91 && str[i]>64)
-                {
-                str[i]=str[i]-7;
-                if(str[i]<64)
-               { b=str[i];
-                c=64-b;
-                str[i]=90-c; }
-                }
-                if(str[i] < 122 && str[i]>96)
-                {str[i]=str[i]-5;
-                if(str[i]<96)
-               { b=str[i];
-                c=96-b;
-                str[i]=122-c;
-        }}
-
-                 L1:
-                 c=7;
-
-        
-        
-        }
-         if(e>0)
-                {
-                        for(j=i-1;j>i-e-1;j--)
-                        if(g<=f)
-                        {
-                        str[j]=st[g];
-                        g++;
-                        }
-                }
-        printf("%s",str);
-        return 0;
-
+int regno;
+char name[20];
+int m1, m2, m3;
+};
+int main()
+{
+struct student s[5];
+struct student *ptr;
+ptr = &s[0];
+int i;
+for (i = 0; i < 5; i++)
+{
+printf("Enter details of Student #%d\n", i + 1);
+printf("Enter Reg no. of student\n");
+scanf("%d", &ptr->regno);
+printf("Enter Name of student\n");
+scanf("%s", ptr->name);
+printf("Enter Marks of student\n");
+scanf("%d %d %d", &ptr->m1, &ptr->m2, &ptr->m3);
+ptr++;
+}
+ptr = &s[0];
+for (i = 0; i < 5; i++)
+{
+printf("\n--------------------------------\n");
+printf("Details of Student #%d\n", i + 1);
+printf("Reg no. of student is %d\n", ptr->regno);
+printf("Name of student is %s\n", ptr->name);
+printf("Marks of student are \n%d %d %d\n", ptr->m1, ptr->m2, ptr->m3);
+ptr++;
+}
+return 0;
 }
