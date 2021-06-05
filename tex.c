e552737240e34a6af816a619bace01c09bc98892
taparia11 <taparianikhil13@gmail.com>
2021-06-05 08:46:43 +0530

    JSON FILE DATA OF DS- ALGO
    .JASON FILE DATA OF DS-ALGO
    +{
        "python.pythonPath": "c:\\Python 3 9\\python.exe"
    
        {
            "configurations": [
                {
                    "name": "Win32",
                    "includePath": [
                        "${workspaceFolder}/**"
                    ],
                    "defines": [
                        "_DEBUG",
                        "UNICODE",
                        "_UNICODE"
                    ],
                    "cStandard": "gnu17",
                    "cppStandard": "c++17",
                    "intelliSenseMode": "windows-gcc-x64"
                }
            ],
            "version": 4
    
    }

--- tex.c
+++ tex.c
@@ -1,4 +1,4 @@
-#include <stdio.h>
+/*#include <stdio.h>
 int main() {
   int t,a[100],i,p=0,j,r,k;
   scanf("%d",&t);
@@ -26,5 +26,31 @@ int main() {
     p=0;
     t--;
     }
+	return 0;
+}*/
+
+#include <stdio.h>
+int main() {
+	int p,q,a,b,c[100],i,j,t;
+  scanf("%d",&t);
+  for(i=0;i<t;i++)
+  {
+    scanf("%d %d ",&a,&b);
+   	for(j=0;j<a;j++)
+      scanf("%d",&c[j]);
+    
+    for(j=0;j<a-1;j++)
+    	if(c[j]>c[j+1])
+          p=c[j];
+    	  c[j]=c[j+1];
+    	  c[j+1]=p;
+    
+   	for(j=0;j<a;j++)
+      printf("%d",c[j]);
+    
+  }
+    
+    
+	
 	return 0;
 }
